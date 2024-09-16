c5ad4db500e67fc6b3252d64820dadda3fac81a2
Yash Raj Singh <yashudps.ys@gmail.com>
2024-09-17 05:14:20 +0530

    feat: header and hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${noto.className} ${noto.className} antialiased`}>
-        {children}
+        <main className="container">
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,39 @@
 import React from "react";
-import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className="container">
-      <Header />
+    <>
       <Hero />
-    </main>
+      <section className="text-center mt-24">
+        <p className="text-gray-600 font-bold">Scheduled without fuss for</p>
+        <div className="flex gap-8 *:h-8 mt-6 justify-center">
+          <img
+            src="https://images.ctfassets.net/lh3zuq09vnm2/7EHGsWMc29UoHErPsvRAsi/0f8210b12ada1e9faa313b4d56274572/Panasonic.svg"
+            alt=""
+          />
+          <img
+            src="https://images.ctfassets.net/lh3zuq09vnm2/7dsuPwH4V8KJvCexSZueZc/272b2ef619de8ae4b443758413a19733/Unbounce_Logo.svg"
+            alt=""
+          />
+          <img
+            src="https://images.ctfassets.net/lh3zuq09vnm2/4Y87kRrhSPSYgUbSWYxP1z/a13177cf43f99e7a79c691c54e271a98/Hubspot.svg"
+            alt=""
+          />
+          <img
+            src="https://images.ctfassets.net/lh3zuq09vnm2/18dCY8kGkSbfdNB2Eod1Pp/4768154e8873caa4c1574499dc4e2aab/Microsoft_Logo.svg"
+            alt=""
+          />
+          <img
+            src="https://images.ctfassets.net/lh3zuq09vnm2/1F2mq86JzeTrvpY7QdJZmh/b29471d788aabbc36a27d5dea6dae917/Nintendo.svg"
+            alt=""
+          />
+          <img
+            src="https://images.ctfassets.net/lh3zuq09vnm2/7siImqwRupwoBHyJCaO1zy/2d7ec1ccc7761f76c8fe18de52e889a4/T-Mobile.svg"
+            alt=""
+          />
+        </div>
+      </section>
+    </>
   );
 }
